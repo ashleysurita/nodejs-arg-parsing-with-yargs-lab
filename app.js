defb0ca22aa7910c2d23845d8fd38d3af5affe9d
Ashley <ashleywong678@gmail.com>
2019-11-27 10:36:49 -0500

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,51 @@
-// Challenge: Use the chalk library in your project
-
-// 1. Install version 2.4.1 of chalk
-// 2. Load chalk into app.js
-// 3. Use it to print the string "Success!" to the console in green
-// 4. Test your work
-// Bonus: Use docs to mess around with other StyleSheet. Make text bold and inverted
-
 const chalk = require('chalk')
+const yargs = require('yargs')
 
-console.log(chalk.inverse.blue('Success!'))
+//customize yargs version
+yargs.version('1.1.0')
+
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function(){
+        console.log('Adding new note')
+    }
+})
+
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing note')
+    }
+})
+
+//create list command
+yargs.command({
+    command: 'list',
+    describe: 'List a note',
+    handler: function(){
+        console.log('Listing all notes')
+    }
+})
+
+//create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log('Reading a note')
+    }
+})
+
+//
+// Challenge: Add 2 new commands
+
+// 1. Setup command to support "list" command (print placeholder message for now)
+// 2. Setup command to support "read" command (print placeholder message for now)
+// 3. Test your work by running both commands and ensure correct output
+
+
+console.log(yargs.argv)
